5129662f1855771081d60078eb5b07a923a7b48c
Oswald Calvario <oswald.calvario@gmail.com>
2020-06-20 14:49:09 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -15,5 +15,6 @@ class StudentsController < ApplicationController
     @student = Student.create(first_name: params[:first_name], last_name: params[:last_name])
     redirect_to student_path(@student
   end
+end 
 
 end
